c8ff69741cd6a91b1a5e114651813a3cfa001314
skjr7 <106689265+skjr7@users.noreply.github.com>
2022-07-04 06:37:02 +0530

    Update alive.py

--- AbishnoiRobot/modules/alive.py
+++ AbishnoiRobot/modules/alive.py
@@ -11,22 +11,22 @@ from AbishnoiRobot import telethn as tbot
 
 
 PHOTO = [
-    "https://telegra.ph/file/c8b388e5c3fa17f7dbbfe.jpg",
-    "https://telegra.ph/file/32c5d35031af56a2bc37e.mp4",
+    "https://telegra.ph/file/40f7da0c6f0dd5915de99.jpg",
+    "https://telegra.ph/file/d637a3e9b891ad9e43365.jpg",
 ]
 
 
 @register(pattern=("/alive"))
 async def awake(event):
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ 「 ᴀʙɢ 𒆜 ʀᴏʙᴏᴛ 」​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝝙𝗕𝗜𝗦𝗛𝗡𝗢𝗜](https://t.me/Abishnoi1M)** \n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝗦𝗨𝗥𝗬𝗔𝗔 𝗞𝗨𝗠𝗔𝗥](https://t.me/Suryaakumar)** \n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/Abishnoi_ro_bot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/Abishnoi_bots"),
+            Button.url("ʜᴇʟᴘ​", "https://t.me/Zaynmalik_gc_bot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/mafia_kings_queens"),
         ]
     ]
     ran = random.choice(PHOTO)
